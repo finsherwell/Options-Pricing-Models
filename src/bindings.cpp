cd49f998cdc8bab7cfdbda28e730379f7f6b07d0
finsherwell <finleysherwell@icloud.com>
2025-11-08 12:06:26 +0000

    Misc Changes
    Updated small parts to some files, added virtual environment, updated requirements.

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -2,9 +2,15 @@
 #include <pybind11/complex.h>
 #include <pybind11/stl.h>
 
-#include "binomial.cpp"
-#include "black_scholes.cpp"
-#include "monte_carlo.cpp"
+#include "src/binomial.hpp"
+#include "src/black_scholes.hpp"
+#include "src/monte_carlo.hpp"
 
 namespace py = pybind11;
 
+PYBIND11_MODULE(opm, m) {
+    m.doc() = "Option Pricing Models (C++ -> Python bindings)";
+
+    m.def("black_scholes_call", &opm::black_scholes_call);
+    m.def("black_scholes_put", &opm::black_scholes_put);
+}
